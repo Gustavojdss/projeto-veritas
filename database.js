323941aa0ff2fe7b08475f70f220e2d7e0e205cf
Gustavojdss <156114291+Gustavojdss@users.noreply.github.com>
2025-11-23 23:52:35 -0300

    Update database.js

--- database.js
+++ database.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,       // NÃO coloque IP fixo aqui
+  // AQUI ESTAVA O ERRO: Antes estava fixo 'localhost' ou '127.0.0.1'
+  // Agora ele vai ler a variável que configuramos no Railway:
+  host: process.env.DB_HOST, 
+  
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306, // Adicione essa linha por segurança
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
